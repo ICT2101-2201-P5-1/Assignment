7bf6c6850e55758e713ab94fde1d11f915afb598
KAH HOWE WOO <46815334+JustAyce@users.noreply.github.com>
2021-11-15 20:39:36 +0800

    Removed username input

--- processLogin.py
+++ processLogin.py
@@ -5,26 +5,23 @@ from wtforms.validators import DataRequired, Length
 from flask_bcrypt import Bcrypt
 from datetime import datetime
 
-# username = "admin"
-# password = "admin"
-
 bcrypt = Bcrypt()
 
+
 class LoginForm(FlaskForm):
-    username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
-    submit = SubmitField('Login')
+    submit = SubmitField('Log In')
     attempt = None
     incident_time = None
 
     def load(self):
         if 'attempt' not in session:
-            self.attempt = session['attempt'] = 4
+            self.attempt = session['attempt'] = 2
         else:
             self.attempt = session['attempt']
-        flash(f"\nNumber of attempts: {self.attempt}\n")
+            if self.attempt <= 0:
+                self.attempt = 0
+        # flash(f"\nNumber of attempts: {self.attempt}\n")
         if 'incident_time' in session:
             self.incident_time = int(session['incident_time'])
             # flash(f"incident time: {int(self.incident_time)}, current_time: {int(datetime.now().timestamp())}, time diff: {int(datetime.now().timestamp()) - int(self.incident_time)}")
@@ -32,22 +29,23 @@ class LoginForm(FlaskForm):
                 self.password.render_kw = {'readonly': True}
             else:
                 session.pop('incident_time', None)
-                self.attempt = session['attempt'] = 5
+                self.attempt = session['attempt'] = 3
         else:
             self.incident_time = 0
 
     def check(self):
         if self.validate_on_submit():
+            self.attempt -= 1
+            session['attempt'] = self.attempt
             hashed_input = bcrypt.generate_password_hash(self.password.data).decode('utf-8')
             if bcrypt.check_password_hash(hashed_input, 'admin') and self.password.data == "admin":
                 flash("Successful Login!", 'success')
                 return True
             else:
-                self.attempt -= 1
-                session['attempt'] = self.attempt
                 if self.attempt < 1:
-                    flash('Too many incorrect logins', 'danger')
                     session['attempt'] = 0
                     if not 'incident_time' in session:
                         session['incident_time'] = datetime.now().timestamp()
+                    flash(f'Too many incorrect logins incident"', 'danger')
+
                 return False
