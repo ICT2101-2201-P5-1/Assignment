6918391026a205baecafea2211fff7b421eb46cc
mayinot <31657679+mayinot@users.noreply.github.com>
2021-11-18 23:59:47 +0800

    added comments for processFile.py

--- Models/processFile.py
+++ Models/processFile.py
@@ -2,12 +2,20 @@ import Models.EditLevel
 
 
 '''
-Edit Level
-    Handle receiving of POST request from Map and rendering of CreateLevel page 
-        @param jsdata The data transfered from drag & drop interface 
-        @param JSON_obj JSON object in position: "2", value:"goal"
-        @param mapList global array 
-        @return the CreateLevel.html page 
+Write To Map File
+    Handle the file processing validate if the dictionary have Goal sprite if 
+    Goal sprite is present insert new Level Map into a text file under levels folder 
+    and call on Model to insert to database.
+        @param CommandList The id list of checked commands
+        @param LevelName String levelName user input
+        @param Difficulty value 1(easy),2(medium),3(hard)
+        @param mapList Array that store position and sprite
+        @param MapDict (Position: Sprite) key and value pair 
+        @param fileName is path to generate new textfile
+        @param dbfilePath used to change formating of level map (coordinate with map rendering)
+        @param fileObj file object
+        @param dbStatus status of insert statement (sucess/fail)
+        @return success/fail
 '''
 def writeToMapFile(Maparray,LevelName,CommandList, Difficulty):
     MapDict ={}
@@ -30,8 +38,22 @@ def writeToMapFile(Maparray,LevelName,CommandList, Difficulty):
 
 
         
-     
-
+'''
+Process Commands 
+    Check the commands provide by user in the list and insert to file 
+    Commands Order:
+        1) Move Upward 
+        2) Move Downward 
+        3) Move Left
+        4) Move Right
+        5) Repeat 
+        6) If wall
+        7) If coins 
+    If command is present it is indicated with 1 in the text file in the order stated above. 
+    The 1st line in the text file is used to indicate commands provided.
+        @param CommandList The id list of checked commands
+        @param fileObj file object
+'''   
 
 def processCommands(fileObj, CommandList):
     for i in range(1,8):
@@ -46,7 +68,14 @@ def processCommands(fileObj, CommandList):
             else:
                 fileObj.write('0')
 
-
+'''
+Process Grid
+    Check the sprites provide by user for each grid and insert to file.
+    The 5x5 grid is inserted as values stated below. 
+    Find Grid Type is used to get repective value of sprite.
+        @param MapDict (Position: Sprite) key and value pair 
+        @param fileObj file object
+''' 
 def processGrid(fileObj, MapDict):
     fileObj.write('1')
     for i in range(2,26):
@@ -62,7 +91,17 @@ def processGrid(fileObj, MapDict):
             else: 
                 fileObj.write('0')
 
-
+'''
+Find Grid Type
+    Values:
+        0 = empty
+        1 = car
+        2 = wall
+        3 = coin
+        4 = goal
+    Each value represent on one box on grid map
+        @param gridType Sprite value
+''' 
 def findGridType(gridType):
     if (gridType == 'wall'):
         return '2'
