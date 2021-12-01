16675e2970e0b416316a303356811316d6a9608e
BestCrochet <alicia159@live.com.sg>
2021-12-01 17:04:19 +0800

    added unit test cases for Login feature.

--- test_processLogin.py
+++ test_processLogin.py
@@ -1,19 +1,56 @@
 # Python Test lib 
 import unittest
 from Models.processLogin import LoginForm
-from wtforms_test import FormTestCase
+from Models.EditLevel import fetchPassword
+from flask import Flask
+from CoolMotor import app
+
 
 class TestFormTestCase(unittest.TestCase):
 
-    
+    def setUp(self):
+        self.app = app
+        self.app.config['TESTING'] = True
+        self.app.config['WTF_CSRF_ENABLED'] = False
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
 
-    def test_load(self):
-        form_class = LoginForm
-        form_class.password = 'May'
-        result =LoginForm.load(form_class)
-        print(result)
+    # checks if LoginForm returns the correct class
+    def test_checkFormClass(self):
+        form_class = LoginForm()
+        self.assertIsInstance(form_class, LoginForm)
+
+    # tests if login page is working properly
+    def test_login_page(self):
+        response = self.client.get('/login')
+        self.assertEqual(response.status_code, 200)
+
+    # helper function to send login POST request
+    def login(self, password):
+        response = self.client.post('/login', data={
+                'password': password
+            })
+        return response
+
+    # tests for login failure.
+    def test_loginFailure(self):
+        res = self.login('rubbishpassword')
+        self.assertIn(b"Wrong Password!", res.data)
+
+    # tests for login success.
+    def test_loginSuccess(self):
+        password = str(fetchPassword())[3:-4]
+        res = self.login(password)
+        self.assertIn(b"/edit_level", res.data)
+
+    # tests the login bruteforcer.
+    def test_loginBrute(self):
+        for i in range(5):
+            res = self.login('rubbishpassword')
+        self.assertIn(b"Too many incorrect logins incident!", res.data)
 
 
-
-
+if __name__ in '__main__':
+    unittest.main()
 
