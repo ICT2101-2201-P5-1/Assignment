da28f4142d9118ec266b4c1a136e924a534f5e03
Shawn <2001401@sit.singaporetech.edu.sg>
2021-12-01 14:16:18 +0800

    Added Ultrasonic Transfer

--- telnetCom.py
+++ telnetCom.py
@@ -10,6 +10,8 @@ def sendCommands(commands):
     print(commands)
     print("Writing data")
     tn.write(commands)
+    data = tn.read_until(b'END')
+    print(data + "Recieved from Car")
     print("DISCONNECT")
 
 def receiveData():
