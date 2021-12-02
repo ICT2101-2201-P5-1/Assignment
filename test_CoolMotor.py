5695a357d4049e6a28e0329628fe87d3dd7def6e
WeirdBalls <22877622+WeirdBalls@users.noreply.github.com>
2021-12-02 16:27:00 +0800

    Tested assigned functions.
    White box testing code for code in displayLevel.py
    and edit_level(),
    view_display_Level()
    and def delete_level(id) in CoolMotor.py by Tudy

--- test_CoolMotor.py
+++ test_CoolMotor.py
@@ -1,5 +1,6 @@
 import unittest
 import CoolMotor
+import json
 from CoolMotor import app
 from flask import Flask, render_template, url_for, flash, redirect, request, jsonify, make_response,session
 '''
@@ -22,19 +23,31 @@ def delete_level(id):
 
 class TestCoolMotor(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
+        self.app = app
+        self.app.config['TESTING'] = True
+        self.app.config['WTF_CSRF_ENABLED'] = False
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
 
     def test_edit_level(self):
-        rv = self.app.get('/edit_level')
+        rv = self.client.get('/edit_level')
         assert rv.status_code == 200
         assert b'<h2 class="map-title"> Edit Map </h2>' in rv.data
 
+    def test_edit_level2(self):
+        x= {"position":"22","type":"goal"}
+        values=json.dumps(x)
+        rv = self.client.post('/edit_level', data={'javascript_data':values})
+        self.assertEqual(rv.status_code, 200)
+        assert b'<h2 class="map-title"> Edit Map </h2>' in rv.data
+
     def test_view_display_Level(self):
-        rv = self.app.get('/displayLevel')
+        rv = self.client.get('/displayLevel')
         assert rv.status_code == 200
         assert b'<th>Map ID</th>' in rv.data
 
     def test_delete_level(self):
-        rv = self.app.post('/deletelevel/5')
+        rv = self.client.post('/deletelevel/5')
         assert rv.status_code == 302
         assert b' redirected automatically to target URL: <a href="/displayLevel">/displayLevel</a>' in rv.data
